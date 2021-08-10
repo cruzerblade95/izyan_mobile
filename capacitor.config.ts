0838355c335dbec92b9aa6cc6b062305d2f5a3bb
NabilAjwad <nrosedi@novatis.com.my>
2021-08-10 18:47:00 +0800

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.crz.ricomtr',
+  appName: 'ricomtr',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
